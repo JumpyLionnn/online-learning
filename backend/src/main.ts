dab2a8d3cb5c213c23b59f5162fddaca51af3fd4
fwipsity <fwipsity@gmail.com>
2021-05-11 19:47:40 +0300

    changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,10 +3,13 @@ const app: ExpressApp = express();
 const http = require("http");
 const server = http.createServer(app);
 const path = require("path");
+const fs = require("fs");
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname +"../../../frontend/index.html"));
 });
+
+
 
 server.listen(3000, () => {
     console.log("listening on *:3000");
