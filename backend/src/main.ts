d50d469de8e943b0e2e4ebdc64ca353f8b5156a8
fwipsity <fwipsity@gmail.com>
2021-05-11 20:44:45 +0300

    proper http server

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,6 +9,29 @@ app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname +"../../../frontend/index.html"));
 });
 
+function walk (directoryName: string): void{
+    const directory: string[] = fs.readdirSync(directoryName);
+
+    for(let i = 0; i < directory.length; i++) {
+        const filePath: string = path.join(directoryName, directory[i]);
+        const stats = fs.statSync(filePath);
+        if (stats.isDirectory()) {
+            walk(filePath);
+        }
+        else{
+            const splittedFileName = directory[i].split(".");
+            if(splittedFileName[splittedFileName.length -1] === "css" || splittedFileName[splittedFileName.length -1] === "js"){
+                app.get(filePath, (req, res) => {
+                    res.sendFile(path.resolve(__dirname +"../../../" + filePath));
+                });
+            }
+        }
+    }
+}
+
+walk("frontend/build");
+
+
 
 
 server.listen(3000, () => {
